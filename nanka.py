f588c3dfd20e78a113da902700048d552919695f
yukumaru <114389767+yukumaru@users.noreply.github.com>
2023-03-08 14:01:00 +0900

    vre2

--- nanka.py
+++ nanka.py
@@ -6,17 +6,17 @@ def calc_distance(x1,y1,x2,y2) :
  diff_y = y1-y2
  return math.sqrt(diff_x**2 + diff_y**2)
 
-ringo_x = random.randrange(0 , 5)
-ringo_y = random.randrange(0 , 5)
+zirai_x = random.randrange(0 , 10)
+zirai_y = random.randrange(0 , 10)
 
-zibun_x = random.randrange(0 , 5)
-zibun_y = random.randrange(0 , 5)
+zibun_x = random.randrange(0 , 10)
+zibun_y = random.randrange(0 , 10)
 
-while(ringo_x != zibun_x ) or (ringo_y != zibun_y) :
+while(zirai_x != zibun_x ) or (zirai_y != zibun_y) :
 
- distance = calc_distance(zibun_x,zibun_y,ringo_x,ringo_y)
- print("りんごまでの道のり",distance)
- yuku = input("w:上に行く s:下に行く d:右に行く a:左に行く")
+ distance = calc_distance(zibun_x,zibun_y,zirai_x,zirai_y)
+ print("地雷までの道のり",distance)
+ yuku = input("w:上に行く s:下に行く d:右に行く a:左に行く q:左斜め上へ行く e:右斜め上へ行く z:左斜め下へ行く c:右斜め下へ行く")
  if yuku == "w" :
   zibun_y = zibun_y - 1
  elif yuku == "s":
@@ -25,4 +25,12 @@ while(ringo_x != zibun_x ) or (ringo_y != zibun_y) :
   zibun_x = zibun_x - 1
  elif yuku == "a":
   zibun_x = zibun_x + 1
-print("りんごの破壊に成功") 
+ elif yuku == "q":
+  zibun_y = (zibun_y - 1)+(zibun_x + 1)
+ elif yuku == "e":
+  zibun_y = (zibun_y - 1)+(zibun_x - 1)
+ elif yuku == "z":
+  zibun_x = (zibun_y - 1)+(zibun_x + 1)
+ elif yuku == "c":
+  zibun_x = (zibun_y + 1)+(zibun_x + 1)
+print("地雷の破壊に成功") 
